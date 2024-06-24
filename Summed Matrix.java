0bc0f33f5951ddae029ed800120e36b69173e743
shashank chaudhary <97469450+Shashankch14@users.noreply.github.com>
2024-06-24 22:36:29 +0530

    Create Summed Matrix.java

---
+++ Summed Matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    static long sumMatrix(long n, long q) {
+        // code here
+         if (q > 2 * n) {    //invalid value of q
+            return 0;
+        }
+        
+        long count = 0;
+        if (q <= n) {
+            count = q - 1;
+        }
+        else {
+            count = 2 * n - q + 1;
+        }
+        
+        return count;
+    }
+}
